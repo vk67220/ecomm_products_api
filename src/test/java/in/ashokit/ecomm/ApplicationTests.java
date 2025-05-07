63d431114167a3da86305a2cbe0d34d1d4bdd4ff
vk67220 <varaprasadkasturi.dba@gmail.com>
2025-05-07 17:45:49 +0530

    Update ApplicationTests.java

--- src/test/java/in/ashokit/ecomm/ApplicationTests.java
+++ src/test/java/in/ashokit/ecomm/ApplicationTests.java
@@ -2,12 +2,14 @@ package in.ashokit.ecomm;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@ActiveProfiles("test")
 class ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
+
